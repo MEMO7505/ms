7a6bdad45f2a1bba77956926becab6e6594b528f
MEMO7505 <mahmoudabdo7505@gmial.com>
2024-06-08 22:45:19 +0300

    ICON

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var productDescInput = document.getElementById("productDescInput")
 var addUpdateBtn = document.getElementById("addUpdateBtn")
 var cancelBtnContainer = document.getElementById("cancelBtnContainer")
 var productsArr = JSON.parse(localStorage.getItem("products")) ?? [];
-var searchInput = document.getElementById ("searchInput")
+var searchInput = document.getElementById("searchInput")
 
 
 displayProducts()
@@ -20,6 +20,8 @@ function addUpdateProduct() {
         addProduct(getProduct())
     } else {
         updateProduct(getProduct())
+        cancelBtnContainer.innerHTML = ""
+
     }
 
     onDataChange()
